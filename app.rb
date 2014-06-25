16492ee3d5b250ebdc63c7b59c5f412d8117b914
v-wang <victorwang920@gmail.com>
2014-06-25 16:58:50 -0400

    finished layout of profile page.  added header and footer with logos.

--- app.rb
+++ app.rb
@@ -3,8 +3,10 @@ require 'sinatra/activerecord'
 require './models'
 require 'bundler/setup'
 require 'rack-flash'
+require 'sinatra/base'
 
 enable :sessions
 use Rack::Flash, :sweep => true
 set :sessions => true
 set :database, "sqlite3:form_app.sqlite3"
+
